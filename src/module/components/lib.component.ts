455482232afa52d96179637941d934c7b425e029
Anthony Nahas <anthony.na@hotmail.de>
2018-06-13 21:05:18 +0200

    chore(package): added the default lib component

---
+++ src/module/components/lib.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mat-component',
+  templateUrl: './lib.component.html',
+  styleUrls: ['./lib.component.scss']
+})
+export class LibComponent {
+  description = 'Set of reusable cards templates built with angular material deisgn';
+}
